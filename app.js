aba4886cef1b886d8b4ad07fde20b01eed6e4e39
justindavidtodd <67066405+justindavidtodd@users.noreply.github.com>
2021-03-01 15:55:07 -0700

    x handlebars

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
 app.set('port', 60633);
 
 app.listen(app.port, function() {
